b2998bbf47c9fccaf777dd9cabd246ea18a4512f
Konstantin <qodunpob@gmail.com>
2024-02-07 23:51:03 +0400

    Fix using className in Section component

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,5 +1,6 @@
 import React, { type PropsWithChildren } from 'react'
 import './Section.css'
+import cn from 'classnames'
 
 export interface SectionProps {
   className?: string
@@ -7,10 +8,17 @@ export interface SectionProps {
 }
 
 export const Section: React.FC<PropsWithChildren<SectionProps>> = ({
+  className,
   title,
   children
 }) => (
-  <div className='component-demo-ui-container component-demo-ui-section'>
+  <div
+    className={cn(
+      className,
+      'component-demo-ui-container',
+      'component-demo-ui-section'
+    )}
+  >
     <h2 className='component-demo-ui-section-title'>{title}</h2>
     {children}
   </div>
